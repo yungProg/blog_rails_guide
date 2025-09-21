3442cdbe578580f3599355290c7adedfcf3b84d6
yungProg <122945393+yungProg@users.noreply.github.com>
2025-09-21 22:30:37 +0000

    Add comment

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
